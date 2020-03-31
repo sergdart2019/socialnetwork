41ed757a23838e23a3a2a87c40ca5862b3c18ab5
serg <usrdjan@gmail.com>
2020-03-31 09:57:44 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'views/view_home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (_) => ViewHome(),
+      },
+    );
+  }
+}
---
+++ lib/views/view_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ViewHome extends StatefulWidget {
+  @override
+  _ViewHomeState createState() => _ViewHomeState();
+}
+
+class _ViewHomeState extends State<ViewHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
